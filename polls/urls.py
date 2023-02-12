5c1b3c4116ceaee9276017d1ee5f54be379baf06
erkinov-khumoyun <erkinovkhumoyun@gmail.com>
2023-02-13 00:58:34 +0500

    .

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,35 @@
 from django.urls import path, include
 from .views import *
-from .apiviews import PollList, PollDetail
+from .apiviews import *
+from rest_framework.routers import DefaultRouter
+from .apiviews import PollVewSet,UserCreate
+
+
+# APIVIEW
 # urlpatterns = [
 #     path("polls/", polls_list, name="polls_list"),
 #     path("polls/<int:pk>/", polls_detail, name="polls_detail")
 # ]
 
 
+# generic and APIVIEWW
+# urlpatterns = [
+#     # path("polls/", PollList.as_view(), name="polls_list"),
+#     # path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+#     # path("choices/", ChoiceList.as_view(), name='choice_list'),
+#     # path("vote/", CreateVote.as_view(), name='create_vote'),
+#     # path("polls/<int:pk>/choices", ChoiceList.as_view(), name='choice_list'),
+#     # path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name='created_vote'),
+#     path('polls/<int:pk>/choices/', ChoiceList.as_view(), name='choice_list'),
+#
+# ]
+
+# ViewSet
+router = DefaultRouter()
+router.register('polls', PollVewSet, basename='polls')
+
 urlpatterns = [
-    path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
+    path('users/', UserCreate.as_view(), name="user_create"),
+
 ]
+urlpatterns += router.urls
