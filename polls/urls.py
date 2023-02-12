1273d250005dcd6f0c92061d6b705bf7cfcc991f
erkinov-khumoyun <erkinovkhumoyun@gmail.com>
2023-02-12 18:02:12 +0500

    .

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from .views import *
+from .apiviews import PollList, PollDetail
+# urlpatterns = [
+#     path("polls/", polls_list, name="polls_list"),
+#     path("polls/<int:pk>/", polls_detail, name="polls_detail")
+# ]
+
 
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
